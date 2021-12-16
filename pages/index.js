3c8f4e40ea9fdd823513fc2026f484c58699cdf9
RabinThapa1998 <imrabin1998@gmail.com>
2021-12-16 17:15:55 +0545

    minors

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>My page title</title>
+        <title>Rabin Bikram Thapa</title>
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
 
